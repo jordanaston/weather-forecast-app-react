e0d62109e73e538eddfece4ccf5af453a2f77742
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-19 14:51:04 +1000

    time and location content built out

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function TimeAndLocation() {
   return (
-    <div>TimeAndLocation</div>
-  )
+    <div>
+      <div className="flex flex-row justify-center items-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 31 May 2022 | Local Time: 12:46 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">
+            Berlin, DE
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
