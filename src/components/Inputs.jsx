43f9cec8b3408c1528585f256a12be197afce4a7
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-19 14:43:43 +1000

    C° and F° buttons added + general formatting

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
+import React from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
 function Inputs() {
   return (
     <div className="flex flex-row justify-center my-6">
-        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
-            <input 
-                type="text" 
-                placeholder="search for city..."
-                className="text-xl font-light p-2 w-full shadow-xl outline-none capitalize placeholder:lowercase"
-            />
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+        <input
+          type="text"
+          placeholder="search for city..."
+          className="text-xl font-light p-2 w-full shadow-xl outline-none capitalize placeholder:lowercase"
+        />
+        <UilSearch
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+        <UilLocationPoint
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+      </div>
 
-        </div>
+      <div className="flex flex-row w-1/4 items-center justify-center">
+        <buttons name="metric" className="text-xl text-white font-light">
+          °C
+        </buttons>
+        <p className="text-xl text-white mx-1">|</p>
+        <buttons name="imperial" className="text-xl text-white font-light">
+          °F
+        </buttons>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Inputs
+export default Inputs;
